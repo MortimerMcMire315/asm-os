10b99ef9bd4cb529941da7d5f12fc3a8ba59c4af
Seth Yoder <seth.a.yoder@gmail.com>
2015-01-01 09:45:28 -0500

    Add cross-compiler binaries.

---
+++ compiler/lib/gcc/i686-elf/4.9.2/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../src/gcc-4.9-20141224/configure --target=i686-elf --prefix=/home/atticus/programming/asm/os/compiler/ --disable-nls --enable-languages=c,c++ --without-headers";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "pentiumpro" } };
